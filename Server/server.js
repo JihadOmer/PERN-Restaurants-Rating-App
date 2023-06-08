74042bffcecef56f912ee9acd812fd428d5e89fc
Jihad Omer <jihadomer940@gmail.com>
2023-06-08 22:43:39 +0100

    Create GET Route

--- Server/server.js
+++ Server/server.js
@@ -4,3 +4,10 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
+
+app.get("/getRestaurants", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    restaurant: "Sareeg",
+  });
+});
