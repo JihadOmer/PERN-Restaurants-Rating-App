2b3bec5284a7e4458bb63cbe3728f181c5ab850a
Jihad Omer <jihadomer940@gmail.com>
2023-06-09 22:46:51 +0100

    Add one restaurant route

--- Server/server.js
+++ Server/server.js
@@ -3,11 +3,26 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
 
-app.get("/getRestaurants", (req, res) => {
+ // get all restaurants
+app.get("/api/v1/restaurants", (req, res) => {
   res.status(200).json({
-    status: "success",
-    restaurant: "Sareeg",
+      status: "success",
+    data :{
+    restaurant: ["Sareeg",'KFC']
+    }
   });
 });
+
+// get individual restaurant
+app.get('/api/v1/getReastaurant/:id',(req, res) => {
+     console.log(req.params)
+})
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
