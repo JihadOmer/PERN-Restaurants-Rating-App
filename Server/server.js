e4ede7bdd995ee7a93d226a36f5c629a1539750c
Jihad Omer <jihadomer940@gmail.com>
2023-06-13 23:56:56 +0100

    updating routes

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,7 @@
-require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan");
+require("dotenv").config();
+
 const app = express();
-const port = process.env.PORT || 3001;
 
 // middleware
 app.use(express.json());
@@ -20,33 +19,43 @@ app.get("/api/v1/restaurants", (req, res) => {
 // get individual restaurant
 app.get("/api/v1/getReastaurant/:id", (req, res) => {
   console.log(req.params);
-});
-
-// create a restaurant
-app.post("/api/v1/createRestaurant", (req, res) => {
-   res.status(201).json({
+  res.status(200).json({
     status: "success",
     data: {
       restaurant: "Sareeg",
     },
+  });
 });
+// create a restaurant
+app.post("/api/v1/createRestaurant", (req, res) => {
+  res.status(201).json({
+    status: "success",
+    data: {
+      restaurant: "Sareeg",
+    },
+  });
 });
 
 // update restaurant
 app.put("/api/v1/updateRestaurant/:id", (req, res) => {
-  res.status(200).json({
+  res.status(201).json({
     status: "success",
-
+    data: {
+      resturant: "Sareeg",
+      location: "Khartoum",
+      rating: "3"
+    },
+  });
 });
-});
-
 
 // delete restaurant
 app.delete("/api/v1/deleteRestaurant/:id", (req, res) => {
   res.status(204).json({
     status: "success",
-
+  });
 });
-});
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server is up and listening on port ${port}`);
+});
