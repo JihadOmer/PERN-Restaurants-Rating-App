642e27f03b05f76b48711f0f131dfb96b091cdb5
Jihad Omer <jihadomer940@gmail.com>
2023-06-16 23:16:16 +0100

    Setup Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,29 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+import UpdatePage from "./routes/UpdatePage";
+
 const App = () => {
-    return <div>App</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route
+            exact
+            path="/restaurants/:id"
+            element={<RestaurantDetailPage />}
+          />
+          <Route
+            exact
+            path="/restaurants/:id/update"
+            element={<UpdatePage />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
