3c8c6ffdb7defbde6bf5994301dbc22494d6b3ff
Jihad Omer <jihadomer940@gmail.com>
2023-06-17 15:13:08 +0100

    Add Header components

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1 className="font-weight-light display-1 text-center">
+        {" "}
+        Restaurant Rating App
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
