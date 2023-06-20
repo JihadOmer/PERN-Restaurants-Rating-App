6ea429b3200e2ecfaf3916638fb1aafc1daaadfc
Jihad Omer <jihadomer940@gmail.com>
2023-06-20 22:47:36 +0100

    Add submit test

---
+++ client/cypress/e2e/Submit button.cy.js
@@ -0,0 +1,5 @@
+cy.get(".btn btn-primary")
+  .type("submit")
+  .click()
+  .get(".btn btn-primary")
+  .click();
--- client/cypress/e2e/inputs.cy.js
+++ client/cypress/e2e/inputs.cy.js
@@ -13,11 +13,7 @@ describe("input", () => {
       .should("have.value", "new Location");
     // price range input
     cy.get(".custom-select").eq(0).select("2").should("have.value", "2");
-    // submit button
-    // cy.get(".btn").eq(0).click();
-    // // check if the new restaurant is added
-    // cy.get(".restaurant").eq(0).should("contain", "new restaurant");
+
+    
   });
 });
-
-// cy.get("custom-select my-1 mr-sm-2").eq(3).type("new price_range");
