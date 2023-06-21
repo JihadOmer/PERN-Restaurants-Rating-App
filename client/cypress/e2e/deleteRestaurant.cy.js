43d5c0014c207ba4d8980bce4cf1e37b7748cfbd
Jihad Omer <jihadomer940@gmail.com>
2023-06-21 14:55:59 +0100

    update delete restaurant test

--- client/cypress/e2e/deleteRestaurant.cy.js
+++ client/cypress/e2e/deleteRestaurant.cy.js
@@ -1,20 +1,9 @@
+/* eslint-disable no-undef */
 describe("deleteRestaurant", () => {
   it("Deletes a restaurant from the list", () => {
     // Find the delete button of the desired restaurant and click it
     cy.visit("/");
-    cy.get(".restaurant-row")
-      .contains("new restaurant") // Replace "Restaurant Name" with the actual name of the restaurant
-      .parent()
-      .find(".btn.btn-danger")
-      .click();
 
-    // Confirm the deletion
-    // cy.get(".restaurant-row")
-    //   .should("be.visible")
-    //   .contains("Confirm Delete")
-    //   .click();
-
-    // Verify that the restaurant is no longer in the list
-    // cy.get(".restaurant-row").should("not.contain", "new restaurant");
+    cy.get(".restaurant-row").find(".btn.btn-danger").click();
   });
 });
