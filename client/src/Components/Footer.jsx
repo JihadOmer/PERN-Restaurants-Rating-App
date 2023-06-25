2fa9ebc38f95578d1acfb09660d82686254d80ac
Jihad Omer <jihadomer940@gmail.com>
2023-06-25 13:09:05 +0100

    Update Footer

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -3,17 +3,17 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <footer>
-        <div className="footer-container">
-          <div className="footer-content">
+      <footer className="footer bg-dark text-light text-center fixed-bottom">
+        <div className="container d-flex justify-content-center align-items-center">
+          <div className="mb-2 mt-2">
             <p>© 2023 Jihad Omer</p>
             <div className="footer-links">
               <a
-                href="https://github.com/JihadOmer"
+                href="https://github.com/JihadOmer/PERN-Restaurants-Rating-App"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                 my GitHub 
+                Code Base
               </a>
               <br />
               <a
