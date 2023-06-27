5f447b5240ba5ba18bda66be4b06c8bef5f55ffb
Jihad Omer <jihadomer940@gmail.com>
2023-06-27 20:45:08 +0100

    add connectionString

--- Server/db/index.js
+++ Server/db/index.js
@@ -4,21 +4,23 @@ const { Pool } = require("pg");
 
 // module.exports = { query: (text, params) => pool.query(text, params) };
 
-// const { Pool } = require("pg");
-// const db = new Pool({
-//   connectionString: process.env.DBConfigLink,
-
-// });
 
 const db = new Pool({
-  user: "user1",
-  host: "dpg-cideb6tph6eounfa10gg-a.frankfurt-postgres.render.com",
-  database: "pricing_jz6y",
-  password: "zi4yMt6QYfzbX4CZmjdrDsWXevNRo1p0", // Make sure it is a string
-  port: "5432",
+  connectionString: process.env.DBConfigLink,
   ssl: {
     rejectUnauthorized: false,
   },
 });
+
+// const db = new Pool({
+//   user: "user1",
+//   host: "dpg-cideb6tph6eounfa10gg-a.frankfurt-postgres.render.com",
+//   database: "pricing_jz6y",
+//   password: "zi4yMt6QYfzbX4CZmjdrDsWXevNRo1p0",
+//   port: "5432",
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
 module.exports = db;
