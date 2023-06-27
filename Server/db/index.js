1d53f9c7fb50375726d8f58df7f474a0e57cbd64
Jihad Omer <jihadomer940@gmail.com>
2023-06-27 15:11:53 +0100

    connect app with remote db

--- Server/db/index.js
+++ Server/db/index.js
@@ -1,7 +1,24 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
+// const pool = new Pool();
 
-module.exports = { query: (text, params) => pool.query(text, params) };
+// module.exports = { query: (text, params) => pool.query(text, params) };
 
+// const { Pool } = require("pg");
+// const db = new Pool({
+//   connectionString: process.env.DBConfigLink,
 
+// });
+
+const db = new Pool({
+  user: "user1",
+  host: "dpg-cideb6tph6eounfa10gg-a.frankfurt-postgres.render.com",
+  database: "pricing_jz6y",
+  password: "zi4yMt6QYfzbX4CZmjdrDsWXevNRo1p0", // Make sure it is a string
+  port: "5432",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+module.exports = db;
