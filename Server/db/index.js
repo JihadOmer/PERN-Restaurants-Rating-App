f3f0e02dd721db8b547ea14fca65309791e1e6d9
Jihad Omer <jihadomer940@gmail.com>
2023-06-28 23:54:58 +0100

    update env

--- Server/db/index.js
+++ Server/db/index.js
@@ -4,19 +4,19 @@ const { Pool } = require("pg");
 
 // module.exports = { query: (text, params) => pool.query(text, params) };
 
-const db = new Pool({
-  connectionString: process.env.DBConnLink,
-});
-
 // const db = new Pool({
-//   user: "user1",
-//   host: "dpg-cideb6tph6eounfa10gg-a.frankfurt-postgres.render.com",
-//   database: "pricing_jz6y",
-//   password: "zi4yMt6QYfzbX4CZmjdrDsWXevNRo1p0",
-//   port: "5432",
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
+//   connectionString: process.env.DBConnLink,
 // });
+
+const db = new Pool({
+  user: "user1",
+  host: "dpg-cideb6tph6eounfa10gg-a.frankfurt-postgres.render.com",
+  database: "pricing_jz6y",
+  password: "zi4yMt6QYfzbX4CZmjdrDsWXevNRo1p0",
+  port: "5432",
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 module.exports = db;
