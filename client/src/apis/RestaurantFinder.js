3f24abd03fd986d6a51a97f9edd5a58b820a93de
Jihad Omer <jihadomer940@gmail.com>
2023-07-04 19:48:49 +0100

    update axious

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -3,5 +3,5 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "restaurants-rating-app.onrender.com/api/v1/restaurants/api/v1/restaurants",
+    "restaurants-rating-app.onrender.com",
 });
