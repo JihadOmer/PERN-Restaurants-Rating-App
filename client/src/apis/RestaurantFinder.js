a3e111a8826ab640fd549ab1c6122eb6b260e2b5
Jihad Omer <jihadomer940@gmail.com>
2023-07-05 12:01:42 +0100

    enable vercel db

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = 'api/v1/restaurants'
-  // process.env.NODE_ENV === "production"
+    // process.env.NODE_ENV === "production"
     // ? "api/v1/restaurants"
     // : "http://localhost:3001/api/v1/restaurants";
 
