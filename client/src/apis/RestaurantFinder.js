f4c8c5d981d0cfdff339e220f249791476f0b4e9
Jihad Omer <jihadomer940@gmail.com>
2023-07-05 14:38:48 +0100

    comment local and node_env

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "production"
-    ? "api/v1/restaurants"
-    : "http://localhost:3001/api/v1/restaurants";
+const baseURL = "api/v1/restaurants";
+// process.env.NODE_ENV === "production"
+// ? "api/v1/restaurants"
+// : "http://localhost:3001/api/v1/restaurants";
 
 export default axios.create({
   baseURL,
