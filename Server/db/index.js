6b097aae68354a67495c72fdd11f8a003cf2e956
Jihad Omer <jihadomer940@gmail.com>
2023-07-07 23:50:45 +0100

    create server only

--- Server/db/index.js
+++ Server/db/index.js
@@ -1,17 +1,23 @@
+// const { Pool } = require("pg");
+
+
+
+// const db = new Pool({
+//   connectionString: process.env.DATABASE_URL + "?sslmode=require",
+// });
+
+// db.connect((err) => {
+//   if (err) throw err;
+//   console.log("🎉 Database connected successfully 🎉");
+// });
+
+// // module.exports = pool;
+// module.exports = { query: (text, params) => db.query(text, params) };
+
+
 const { Pool } = require("pg");
 
-
-
-const db = new Pool({
-  connectionString: process.env.DATABASE_URL + "?sslmode=require",
-});
-
-db.connect((err) => {
-  if (err) throw err;
-  console.log("🎉 Database connected successfully 🎉");
-});
-
-// module.exports = pool;
-module.exports = { query: (text, params) => db.query(text, params) };
-
-
+const pool = new Pool();
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
