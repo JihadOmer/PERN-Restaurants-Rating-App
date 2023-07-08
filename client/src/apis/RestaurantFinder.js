1cfca03f8491c25a952220c227de4bda049f54e5
Jihad Omer <jihadomer940@gmail.com>
2023-07-08 14:59:53 +0100

    index.js

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseURL = "/api/v1/restaurants";
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "api/v1/restaurants/"
+    : "http://localhost:3001/api/v1/restaurants";
 
 export default axios.create({
   baseURL,
