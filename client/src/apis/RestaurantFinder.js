f6cc9f4ecc164d3feaad7eb8b8e30e2c6045247b
Jihad Omer <jihadomer940@gmail.com>
2023-07-12 23:28:50 +0100

    update production env

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -2,10 +2,8 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "restaurants-rating-app.onrender.com/api/v1/restaurants"
+    ? "api/v1/restaurants"
     : "http://localhost:3001/api/v1/restaurants";
-
-    
 
 export default axios.create({
   baseURL,
