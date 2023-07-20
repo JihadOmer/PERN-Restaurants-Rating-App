9f7ac9b24af0512a79fe16666da5d4b97a49ca27
Jihad Omer <jihadomer940@gmail.com>
2023-07-20 19:48:55 +0100

    updaet .env file and index db

--- Server/db/index.js
+++ Server/db/index.js
@@ -1,14 +1,14 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
+const { DATABASE_URL } = process.env;
 const db = new Pool({
-  connectionString: process.env.DATABASE_URL + "?sslmode=require",
+  connectionString: DATABASE_URL + "?sslmode=require",
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log("🎉 Database connected successfully 🎉");
-});
-
+// db.connect((err) => {
+//   if (err) throw err;
+//   console.log("🎉 Database connected successfully 🎉");
+// });
 
 module.exports = db;
-
