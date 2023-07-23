ffac7f814d2308d98f99c14e791ea4bcbd5d2540
Jihad Omer <jihadomer940@gmail.com>
2023-07-23 17:32:11 +0100

    Create reiews API

--- Server/server.js
+++ Server/server.js
@@ -37,20 +37,23 @@ app.get("/api/v1/restaurants", async (req, res) => {
 app.get("/api/v1/restaurants/:id", async (req, res) => {
   try {
     const restaurantId = parseInt(req.params.id); // Convert to integer
-    // Check if the ID is an integer
-    // if (isNaN(restaurantId)) {
-    //     throw new Error("Invalid restaurant ID"); // Throw an error for non-integer input
-    //   }
-    // use this format to avoid sql injection attacks => read more
-    const results = await db.query("select * from restaurant where id = $1", [
-      restaurantId,
-    ]);
+
+    const restaurant = await db.query(
+      "select * from restaurant where id = $1",
+      [restaurantId]
+    );
+    
+    const review = await db.query(
+      "select * from reviews where restaurant_id = $1",
+      [restaurantId]
+    );
 
     res.status(200).json({
       status: "success",
 
       data: {
-        restaurant: results.rows[0], // since we are returning only one restaurant
+        restaurant: restaurant.rows[0], // since we are returning only first restaurant on the array
+        reviews: review.rows,
       },
     });
   } catch (err) {
