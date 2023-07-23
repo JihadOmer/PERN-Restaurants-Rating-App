5de320ed993cbe13e0b837358a7a77c2c0155fde
Jihad Omer <jihadomer940@gmail.com>
2023-07-23 17:32:41 +0100

    Update DataBase Entry

--- Server/db/sb.sql
+++ Server/db/sb.sql
@@ -1,7 +1,39 @@
+
+-- // create restaurant table in database
+
+
+create table restaurant(
+id BIGSERIAL not null PRIMARY KEY,
+name VARCHAR(50) not null,
+location VARCHAR(50) not null,
+price_range int not null
+);
+
+-- insert into restaurant table 
+
+INSERT INTO restaurant(name, location, price_range)
+VALUES('Sareeg','Khartoum', 3);
+
+
+-- // create reviews table 
+
 CREATE TABLE reviews (
-    id BIGINT NOT NULL PRIMARY KEY,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
     restaurant_id BIGINT NOT NULL REFERENCES restaurant(id),
     name VARCHAR(50) NOT NULL,
     review TEXT NOT NULL,
-    rating INT NOT NULL check(rating >= 1 and rating <= 5)
-    );
+    rating INT NOT NULL check(
+        rating >= 1
+        and rating <= 5
+    )
+);
+
+-- insert into reviews table 
+
+INSERT INTO reviews(restaurant_id, name, review, rating)
+VALUES(1, 'jihad', 'so good ',3);
+
+
+
+
+
