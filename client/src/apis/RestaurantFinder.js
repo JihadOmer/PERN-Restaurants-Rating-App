01e8fb3527007593306b48870d9e73dd7fbefb0a
Jihad Omer <jihadomer940@gmail.com>
2023-07-27 21:53:30 +0100

    Update url to work in borh local and production

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const baseURL =
-  "https://test-restaurant-rating-app.onrender.com/api/v1/restaurants";
-
+  process.env.NODE_ENV === "production"
+    ? "https://test-restaurant-rating-app.onrender.com/api/v1/restaurants"
+    : "http://localhost:3001/api/v1/restaurants";
 
 export default axios.create({
   baseURL,
